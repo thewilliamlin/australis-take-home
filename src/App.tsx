a1dc2c7ae65cdd31432d782feb08869575898e85
Cooper Holt <coopergoldenholt@outlook.com>
2025-05-29 16:04:15 -0600

    first push

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to the Challenge!</h1>
+      <p>Start building here.</p>
+    </div>
+  );
+}
+
+export default App;
